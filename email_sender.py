f52ffb5c00cade8a21f699ce3318e2f790d46714
samdunk47 <98485063+samdunk47@users.noreply.github.com>
2022-11-30 20:43:27 +0000

    asef

--- email_sender.py
+++ email_sender.py
@@ -1,6 +1,23 @@
 import os
+import smtplib
+from email.message import EmailMessage
 
-email_username = os.environ.get("alt_email_username")
-email_password = os.environ.get("alt_email_password")
+email_address = os.environ.get("alt_email_username")
+email_password = os.environ.get("alt_email_password_app")
 
+
+msg = EmailMessage()
+msg["Subject"] = "test message 2"
+msg["From"] = email_address
+msg["To"] = "splashtiger117@gmail.com"
+msg.set_content("This is another test message from me")
+
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    
+    smtp.login(email_address, email_password)
+
+    smtp.send_message(msg)
 
